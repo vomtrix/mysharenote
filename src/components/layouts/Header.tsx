5b4e4458ea77fa92bba0af2d417a18c80235e9b9
entanglo <30041918+entanglo@users.noreply.github.com>
2025-10-08 23:20:06 +0200

    add sharenote link

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,11 @@
 import LanguageSwitcher from '@components/common/LanguageSwitcher';
 import SocialLinks from '@components/common/SocialLinks';
 import Connect from '@components/Connect';
-import { Box } from '@mui/material';
+import { FAQ_LINKS } from '@config/config';
+import { Box, Link as MuiLink } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import { SECONDARY_COLOR } from '@styles/colors';
 import styles from '@styles/scss/Header.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -20,8 +22,8 @@ const Header = () => {
               src="/assets/logo.svg"
               alt="Mobile Logo"
               className={styles.mobileLogo}
-              width={100}
-              height={64}
+              width={120}
+              height={48}
             />
           </Link>
           <Link href="/" passHref>
@@ -29,7 +31,7 @@ const Header = () => {
               src="/assets/logo.svg"
               alt="Logo"
               className={styles.logo}
-              width={128}
+              width={170}
               height={64}
             />
           </Link>
@@ -37,6 +39,14 @@ const Header = () => {
         <Connect />
 
         <div className={styles.rightContent}>
+          <MuiLink
+            sx={{ pr: 2, display: { xs: 'none', md: 'block' } }}
+            href={FAQ_LINKS.shareNote}
+            target="_blank"
+            rel="noopener noreferrer"
+            color={SECONDARY_COLOR}>
+            {t('header.shareNote')}
+          </MuiLink>
           <SocialLinks />
           <LanguageSwitcher />
         </div>
